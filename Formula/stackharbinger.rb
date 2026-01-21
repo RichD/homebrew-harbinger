f6d83c71fa00017d9c922118655f153042eff028
Rich Dabrowski <engineering@richd.net>
2026-01-20 18:09:05 -0800

    bump stackharbinger to 1.0.1

--- Formula/stackharbinger.rb
+++ Formula/stackharbinger.rb
@@ -1,8 +1,8 @@
 class Stackharbinger < Formula
   desc "Track End-of-Life dates for your tech stack"
   homepage "https://stackharbinger.com"
-  url "https://rubygems.org/downloads/stackharbinger-1.0.0.gem"
-  sha256 "5735f5e07eebe4f519390aef7493f1eff4b1d475bb750e559b4f21632a10e74f"
+  url "https://rubygems.org/downloads/stackharbinger-1.0.1.gem"
+  sha256 "eca13857bd5b60f4dbca6485eed11e70be6c7267ce2a4c0db9634b0b79912ffe"
   license "MIT"
 
   depends_on "ruby" => "3.1"
@@ -16,6 +16,6 @@ class Stackharbinger < Formula
   end
 
   test do
-    assert_match "Harbinger version 1.0.0", shell_output("#{bin}/harbinger version")
+    assert_match "Harbinger version 1.0.1", shell_output("#{bin}/harbinger version")
   end
 end
